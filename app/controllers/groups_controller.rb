b95549c8fc4c97f7321c4616275983d216bb25dd
tomohumi1109 <tomofumi.asnao@di-v.co.jp>
2019-08-28 09:40:37 +0900

    groups-index,controller-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   def index
   end
-
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,6 +18,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end

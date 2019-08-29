0994dadcefe54b0ce438a818b95450dd02bdd94e
tomohumi1109 <tomofumi.asnao@di-v.co.jp>
2019-08-29 14:06:10 +0900

    send-massage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
   before_action :set_group
+
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
@@ -12,6 +15,12 @@ class MessagesController < ApplicationController
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group

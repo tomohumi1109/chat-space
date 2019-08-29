56f4c15b94fa5bb606b2d3ac81db1862dbb4aa56
tomohumi1109 <tomofumi.asnao@di-v.co.jp>
2019-08-29 13:26:28 +0900

    massagesend

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
-
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
